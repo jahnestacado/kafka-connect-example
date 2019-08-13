e68fed48d120d4b1c424662e59d04bb5df24054f
yannis <yannis.tzanellis@ing.com>
2019-08-13 14:52:59 +0200

    Added avro producers and error handling config

---
+++ producer/producer-1.py
@@ -0,0 +1,28 @@
+import io
+import random
+import avro.schema
+import time
+from avro.io import DatumWriter
+from confluent_kafka import avro
+from confluent_kafka.avro import AvroProducer
+
+# Kafka topic
+TOPIC = "topic-1"
+
+# Path to user.avsc avro schema
+SCHEMA_PATH = "message-1.avsc"
+SCHEMA = value_schema = avro.loads(open(SCHEMA_PATH).read())
+
+avroProducer = AvroProducer({
+    'bootstrap.servers': '192.168.1.104:19092',
+    'schema.registry.url': 'http://192.168.1.104:8085'
+    },  default_value_schema=SCHEMA)
+
+
+while True:
+    value={"key": "key-" + str(random.randint(0, 10)), "value": random.randint(0, 1000)}
+    print(value)
+    avroProducer.produce(topic=TOPIC, value=value)
+    time.sleep(random.randint(0, 3))
+
+avroProducer.flush()
